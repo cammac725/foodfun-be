bcafb1ce3f3cb828b8a923a17b32c0e299837077
cammac <camm@rocktechcomputer.com>
2020-03-04 16:02:39 -0800

    comment out production

--- knexfile.js
+++ knexfile.js
@@ -37,23 +37,23 @@ module.exports = {
       }
   },
 
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL, 
-    // || {
-    //   database: "process.env.DB",
-    //   user: "username",
-    //   password: "password"
-    // },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  }
+  // production: {
+  //   client: "pg",
+  //   connection: process.env.DATABASE_URL, 
+  //   // || {
+  //   //   database: "process.env.DB",
+  //   //   user: "username",
+  //   //   password: "password"
+  //   // },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: "./data/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds"
+  //   }
+  // }
 };
